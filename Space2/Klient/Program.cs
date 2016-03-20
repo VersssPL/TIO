257ac7b7f722c151548e895ccbccb7b586ab13d1
VersssPL <majtyka.m@gmail.com>
2016-03-21 00:22:03 +0100

    first commit

---
+++ Space2/Klient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Klient.ServiceReference1;
+
+/* Michał Majtyka - lab2 */
+
+namespace Klient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlackHoleClient BlackHoleClient = new BlackHoleClient();
+
+            Person kapitan = new Person() { Name = "Kapitan Hak", Age = 40 };
+            Person[] zaloga = new Person[] { new Person() { Name = "Piotrus Pan", Age = 18 }, new Person() { Name = "Wendy", Age = 18 }, new Person() { Name = "Pan Smee", Age = 60 }, new Person() { Name = "Jas", Age = 10 }, new Person() { Name = "Krokodyl", Age = 10 } };
+            Starship ship = new Starship() { Name = "Statek", Captain = kapitan, Crew = zaloga };
+
+            Console.WriteLine("Załoga statku:");
+            PresentCrew(ship);
+            Console.WriteLine();
+
+            Console.WriteLine("Ultimate answer: " + BlackHoleClient.UltimateAnswer());
+            Console.WriteLine();
+
+            Console.WriteLine("Załoga statku po kontakcie z czarną dziurą:");
+            PresentCrew(BlackHoleClient.PullStarship(ship));
+
+            Console.ReadKey();
+        }
+
+        static void PresentCrew(Starship ship)
+        {
+            Console.WriteLine("Kapitan: " + ship.Captain.Name + " (" + ship.Captain.Age + " lat)");
+            foreach (var person in ship.Crew)
+            {
+                Console.WriteLine(person.Name + " (" + person.Age + " lat)");
+            }
+        }
+    }
+}
---
+++ Space2/Space2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+/* Michał Majtyka - lab2 */
+
+namespace Space2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri address = new Uri("http://localhost:8000/BlackHole");
+            ServiceHost selfHost = new ServiceHost(typeof(BlackHole), address);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IBlackHole), new WSHttpBinding(), "ServiceEndpoint");
+                ServiceMetadataBehavior smd = new ServiceMetadataBehavior();
+                smd.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smd);
+
+                selfHost.Open();
+                Console.WriteLine("Service is running!");
+                Console.ReadLine();
+
+                selfHost.Close();
+            }
+            catch (CommunicationException)
+            {
+                Console.WriteLine("Connectivity problem...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                selfHost.Abort();
+            }
+        }
+
+
+    }
+
+    [DataContract]
+    public class Person
+    {
+
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+
+    }
+
+    [DataContract]
+    public class Starship
+    {
+
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public Person Captain { get; set; }
+        [DataMember]
+        public List<Person> Crew { get; set; }
+    }
+
+    [DataContract]
+    public class Planet
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Mass { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IBlackHole
+    {
+        [OperationContract]
+        Starship PullStarship(Starship ship);
+        [OperationContract]
+        string UltimateAnswer();
+    }
+
+    class BlackHole : IBlackHole
+    {
+        public Starship PullStarship(Starship ship)
+        {
+            if (ship.Captain.Age <= 40)
+            {
+                foreach (var person in ship.Crew)
+                    person.Age += 20;
+            }
+            return ship;
+        }
+        public string UltimateAnswer()
+        {
+            return (42.ToString());
+        }
+
+    }
+}
