de52e59f918e60a49751948d0fee35f611c274e9
VersssPL <majtyka.m@gmail.com>
2016-04-18 17:06:30 +0200

    Added files via upload
    AuthorsDB

---
+++ zad_5/AuthorsDBActions/AuthorsRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+using LibraryBookAuthor;
+
+namespace AuthorsDBActions
+{
+    public class AuthorsRepository : IAuthorsRepository
+    {
+        private readonly string _authorsConnection = @"C:\tmp\authors";
+
+        public List<Author> GetAll()
+        {
+            using (var db = new LiteDatabase(this._authorsConnection))
+            {
+                var repository = db.GetCollection<Author>("authors");
+                var results = repository.FindAll();
+                return results.ToList();
+            }
+        }
+
+        public int Add(Author author)
+        {
+            using (var db = new LiteDatabase(this._authorsConnection))
+            {
+                var dbObject = author;
+                var repository = db.GetCollection<Author>("authors");
+                if (repository.FindById(author.Id) != null)
+                {
+                    repository.Update(dbObject);
+                }
+                else
+                {
+                    repository.Insert(dbObject);
+                }
+
+                return dbObject.Id;
+            }
+        }
+
+        public Author Get(int id)
+        {
+            using (var db = new LiteDatabase(this._authorsConnection))
+            {
+                var repository = db.GetCollection<Author>("authors");
+                var result = repository.FindById(id);
+                return result;
+            }
+        }
+
+        public Author Update(Author author)
+        {
+            using (var db = new LiteDatabase(this._authorsConnection))
+            {
+                var repository = db.GetCollection<Author>("authors");
+                var dbObject = author;
+                if (repository.Update(dbObject))
+                {
+                    return dbObject;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            using (var db = new LiteDatabase(this._authorsConnection))
+            {
+                var repository = db.GetCollection<Author>("authors");
+                return repository.Delete(id);
+            }
+        }
+
+    }
+}
---
+++ zad_5/AuthorsDBActions/IAuthorsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryBookAuthor;
+
+namespace AuthorsDBActions
+{
+    public interface IAuthorsRepository
+    {
+        List<Author> GetAll();
+        int Add(Author author);
+        Author Get(int id);
+        Author Update(Author author);
+        bool Delete(int id);
+    }
+}
