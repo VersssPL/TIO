6bfd7d66407fb0baaadcd4ee75b1984e01206d2b
VersssPL <majtyka.m@gmail.com>
2016-04-18 17:10:21 +0200

    Added files via upload

---
+++ zad_5/RESTfulService/Controllers/AuthorsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LibraryBookAuthor;
+using AuthorsDBActions;
+
+namespace RESTfulService.Controllers
+{
+    public class AuthorsController : ApiController
+    {
+        // GET: api/Authors
+        public IEnumerable<Author> Get()
+        {
+            AuthorsRepository authorsRepo = new AuthorsRepository();
+            return authorsRepo.GetAll();
+        }
+
+        // GET: api/Authors/5
+        public Author Get(int id)
+        {
+            AuthorsRepository authorsRepo = new AuthorsRepository();
+            return authorsRepo.Get(id);
+        }
+
+        // POST: api/Authors
+        public int Post([FromBody]Author author)
+        {
+            AuthorsRepository authorsRepo = new AuthorsRepository();
+            return authorsRepo.Add(author);
+        }
+
+        // PUT: api/Authors/5
+        public Author Put(int id, [FromBody]Author author)
+        {
+            AuthorsRepository authorsRepo = new AuthorsRepository();
+            author.Id = id;
+            return authorsRepo.Update(author);
+        }
+
+        // DELETE: api/Authors/5
+        public bool Delete(int id)
+        {
+            AuthorsRepository authorsRepo = new AuthorsRepository();
+            return authorsRepo.Delete(id);
+        }
+    }
+}
---
+++ zad_5/RESTfulService/Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LibraryBookAuthor;
+using BooksDBActions;
+
+namespace RESTfulService.Controllers
+{
+    public class BooksController : ApiController
+    {
+        // GET: api/Books
+        public IEnumerable<Book> Get()
+        {
+            BooksRepository booksRepo = new BooksRepository();
+            return booksRepo.GetAll();
+        }
+
+        // GET: api/Books?search=bookTitle
+        public IEnumerable<Book> Get([FromUri]string search)
+        {
+            BooksRepository booksRepo = new BooksRepository();
+            return booksRepo.GetAll().Where(x => x.BookTitle.Contains(search));
+        }
+
+        // GET: api/Books/5
+        public Book Get(int id)
+        {
+            BooksRepository booksRepo = new BooksRepository();
+            return booksRepo.Get(id);
+        }
+
+        // POST: api/Books
+        public int Post([FromBody]Book book)
+        {
+            BooksRepository booksRepo = new BooksRepository();
+            return booksRepo.Add(book);
+        }
+
+        // PUT: api/Books/5
+        public Book Put(int id, [FromBody]Book book)
+        {
+            BooksRepository booksRepo = new BooksRepository();
+            book.Id = id;
+            return booksRepo.Update(book);
+        }
+
+        // DELETE: api/Books/5
+        public bool Delete(int id)
+        {
+            BooksRepository booksRepo = new BooksRepository();
+            return booksRepo.Delete(id);
+        }
+    }
+}
