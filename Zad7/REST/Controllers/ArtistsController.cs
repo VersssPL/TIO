1d976c9c5b0cc1f57d682a21c554d92193aba8e7
VersssPL <majtyka.m@gmail.com>
2016-05-10 18:02:15 +0200

    Add files via upload

---
+++ Zad7/REST/Controllers/ArtistsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ArtDB;
+using ArtLibrary;
+
+namespace REST.Controllers
+{
+    public class ArtistsController : ApiController
+    {
+        private IArtistsRepository db;
+        // GET: api/Artists
+        public IEnumerable<Artist> Get()
+        {
+            return db.GetAll();
+        }
+
+        // GET: api/Artists/5
+        public Artist Get(int id)
+        {
+            return db.Get(id);
+        }
+
+        // POST: api/Artists
+        public void Post([FromBody]Artist value)
+        {
+            db.Add(value);
+        }
+
+        // PUT: api/Artists/5
+        public void Put(int id, [FromBody]Artist value)
+        {
+            db.Update(value);
+        }
+
+        // DELETE: api/Artists/5
+        public void Delete(int id)
+        {
+            db.Delete(id);
+        }
+    }
+}
---
+++ Zad7/REST/Controllers/PaintingsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ArtDB;
+using ArtLibrary;
+
+namespace REST.Controllers
+{
+    public class PaintingsController : ApiController
+    {
+        private IPaintingsRepository db;
+        // GET: api/Paintings
+        public IEnumerable<Painting> Get()
+        {
+            return db.GetAll();
+        }
+
+        // GET: api/Paintings/5
+        public Painting Get(int id)
+        {
+            return db.Get(id);
+        }
+
+        // POST: api/Paintings
+        public void Post([FromBody]Painting value)
+        {
+            db.Add(value);
+        }
+
+        // PUT: api/Paintings/5
+        public void Put(int id, [FromBody]Painting value)
+        {
+            db.Update(value);
+        }
+
+        // DELETE: api/Paintings/5
+        public void Delete(int id)
+        {
+            db.Delete(id);
+        }
+    }
+}
